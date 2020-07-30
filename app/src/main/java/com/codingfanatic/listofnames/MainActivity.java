aafa633fc44580d90b0e4b508eaa5ef23a56b7f0
codingfanatic <lclarkerichard@yahoo.com>
2020-07-30 11:00:36 -0400

    Writing to a database Added code to MainActivity for writing data to the Realtime Database

---
+++ app/src/main/java/com/codingfanatic/listofnames/MainActivity.java
@@ -0,0 +1,23 @@
+package com.codingfanatic.listofnames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("cheesey");
+
+        myRef.setValue("I made this myself NOT");
+
+    }
+}
