858ccbf1ddb3fe9e555f382faf8d36349ccb3757
codingfanatic <lclarkerichard@yahoo.com>
2020-08-20 02:21:16 -0400

    Attempted to populate a ListView using Firebase Added a Listview to the MainActivity Created a class (Person) to represent the details of the ListView Referenced the ListView in the MainActivity Researched the class needed to import FirebaseListAdapter objects Created the xml file to style the ListView and refactored code

--- app/src/main/java/com/codingfanatic/listofnames/MainActivity.java
+++ app/src/main/java/com/codingfanatic/listofnames/MainActivity.java
@@ -10,11 +10,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import
+import android.view.View;
+import
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+
+        /*
+         * Create a DatabaseReference to the data; works with standard DatabaseReference methods
+         * like limitToLast() and etc.
+         */
+        DatabaseReference myRef = database.getReference().child("message");
 
         //myRef.setValue("I want to read this into my app");
 
@@ -32,8 +43,6 @@ public class MainActivity extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue(String.class);
-                TextView mainTV = findViewById(R.id.mainTextView);
-                mainTV.setText(value);
                 Log.d(TAG, "Value is: " + value);
             }
         
@@ -43,6 +52,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+
+         // Find the ListView
+         mListView = (ListView) findViewById(R.id.list_view);
+
+         // Now set the adapter with a given layout
+         mListView.setAdapter(new FirebaseListAdapter<Person>(this, Person.class,
+                 R.layout.list_listview, myRef) {
+ 
+             // Populate view as needed
+             protected void populateView(View view, Person person, int position) {
+                 ((TextView) view.findViewById(android.R.id.text1)).setText(person.getName());
+             }
+         });
 
     }
 }
---
+++ app/src/main/java/com/codingfanatic/listofnames/Person.java
@@ -0,0 +1,14 @@
+package com.codingfanatic.listofnames;
+
+public class Person{
+    private String name;
+
+    public Person(){
+
+    }
+
+    public String getName(){
+
+        return this.name;
+    }
+}
